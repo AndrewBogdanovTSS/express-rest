cc91626ce60552c137b6648eae6b39595c27d87c
DESKTOP-A6D3HI8\Andrew <andrewbogdanov@ukr.net>
2017-09-14 16:29:48 +0300

    connecting database, adding mongoose and implementing get behavior

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express'
-import {booksRouter} from './routes/books'
 import mongoose from 'mongoose'
+import {booksRouter} from './routes/books'
+
 
 const app = express();
 const port = process.env.PORT || 5001;
+const db = mongoose.connect('mongodb://localhost/book_db');
+const bookModel =
 
 app.use('/api', booksRouter);
 
--- routes/books.js
+++ routes/books.js
@@ -1,13 +1,34 @@
 import express from "express";
-
-const app = express();
+import BookModel from '../models/book'
 
 export const booksRouter = express.Router();
 
 booksRouter.route('/books')
-    .get((req, res)=> {
-        res.json({
-            hello: 'This is my API'
+    .get((req, res) => {
+        let filterArr = ['genre', 'author'];
+        let query = {};
+        for (var i = 0; i < filterArr.length; i++) {
+            let criteria = filterArr[i];
+            if (req.query[criteria]) query[criteria] = req.query[criteria];
+        }
+        BookModel.find(query, (err, books) => {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.json(books);
+            }
         });
+
     });
 
+booksRouter.route('/books/:id')
+    .get((req, res) => {
+        BookModel.findById(req.params.id, (err, book) => {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.json(book);
+            }
+        });
+
+    });
