c73dfcfc1fb4fff92dce1dfa0d59307d35a1f657
DESKTOP-A6D3HI8\Andrew <andrewbogdanov@ukr.net>
2017-09-30 17:34:45 +0300

    remove unneeded db dependencie (monggose-connect-db)

--- db.js
+++ db.js
@@ -1,32 +1,42 @@
 import mongoose from 'mongoose'
-import connectDb from 'mongoose-connect-db'
 
 const testPath = process.env.NODE_ENV === 'test' ? '_test' : '';
 const dbURI = 'mongodb://localhost/book_db' + testPath;
 
 mongoose.Promise = global.Promise;
 
-connectDb(mongoose, dbURI);
+mongoose.connect(dbURI, {
+    useMongoClient: true
+}).then(
+    () => {
+        // When successfully connected
+        console.log('Mongoose default connection open to ' + dbURI);
+    },
+    (err) => {
+        console.log('Mongoose default connection error: ' + err);
+    }
+);
 
 // CONNECTION EVENTS
+// First two events can be replaced with a Promise. Example above.
 // When successfully connected
-mongoose.connection.on('connected', function () {
+/*mongoose.connection.on('connected', () => {
     console.log('Mongoose default connection open to ' + dbURI);
 });
 
 // If the connection throws an error
-mongoose.connection.on('error',function (err) {
+mongoose.connection.on('error', (err) => {
     console.log('Mongoose default connection error: ' + err);
-});
+});*/
 
 // When the connection is disconnected
-mongoose.connection.on('disconnected', function () {
+mongoose.connection.on('disconnected', () => {
     console.log('Mongoose default connection disconnected');
 });
 
 // If the Node process ends, close the Mongoose connection
-process.on('SIGINT', function() {
-    mongoose.connection.close(function () {
+process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
         console.log('Mongoose default connection disconnected through app termination');
         process.exit(0);
     });
